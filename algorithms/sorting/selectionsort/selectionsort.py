09214107bd274c5c14ddd340cf184abd1d7a66f2
Abhishek singh <60599305+don-quixotee@users.noreply.github.com>
2020-06-01 18:20:08 +0530

    Update selectionsort.py

--- algorithms/sorting/selectionsort/selectionsort.py
+++ algorithms/sorting/selectionsort/selectionsort.py
@@ -1,25 +1,18 @@
 
+def selectionSort(a):
+    n = len(a)
+    for i in range(0, n-1):
+        min = i
+        for j in range(i+1, n):
+            if a[j]<a[min]:
+                min = j
+                
+        if min != i:
+            a[i],a[min] = a[min], a[i]
+            
 
-def selectionSort(num):
-    for i in range(len(num)):
-        min_idx = i
-        for j in range(i+1, len(num)):
-            if num[min_idx] > num[j]:
-                min_idx = j
-
-        num[i],num[min_idx] = num[min_idx], num[i]
-    return num
+a = [7,4,10,8,3,1]
+selectionSort(a)
+print(a)
 
 
-
-
-n = [2,4,1,3,6,7,5,6,9]
-print(selectionSort(n))
-
-
-"""
-Time Complexity: O(n2) as there are two nested loops.
-
-Auxiliary Space: O(1)
-The good thing about selection sort is it never makes more than O(n) swaps and can be useful when memory write is a costly operation.
-"""
